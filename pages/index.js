1d45bbf4dc217a1a2bfe48c309b7b402e85a5ed7
Adam Butcher <adabutch1@gmail.com>
2022-01-14 09:29:10 -0500

    hello adam

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,9 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Adam Butcher
         </h1>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
 
         <div className="grid">
           <a href="https://nextjs.org/docs" className="card">
